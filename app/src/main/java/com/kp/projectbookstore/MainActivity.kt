dc2e3c87d58a4669065c548e9b2e7c36295a7614
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-03-15 09:58:32 +0200

    Етап 3: відображення списку книг на головному екрані

--- app/src/main/java/com/kp/projectbookstore/MainActivity.kt
+++ app/src/main/java/com/kp/projectbookstore/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.kp.projectbookstore
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kp.projectbookstore.adapters.BookAdapter
+import com.kp.projectbookstore.data.TestData
+import com.kp.projectbookstore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bookAdapter: BookAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupRecyclerBiew()
+    }
+
+    private fun setupRecyclerBiew() {
+        bookAdapter = BookAdapter(TestData.books) {
+            selectedBook -> Toast.makeText(this, "Selected: ${selectedBook.title}", Toast.LENGTH_SHORT).show()
         }
+
+        binding.rvBooks.layoutManager = LinearLayoutManager(this)
+        binding.rvBooks.adapter = bookAdapter
     }
 }
