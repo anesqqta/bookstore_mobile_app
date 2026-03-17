942d36c9846b1a001bfd85e96cb17c2f4b061cc9
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-03-17 22:03:53 +0200

    Етап 4: додавання екранів заповнювачів для навігації

---
+++ app/src/main/java/com/kp/projectbookstore/activities/ProfileActivity.kt
@@ -0,0 +1,20 @@
+package com.kp.projectbookstore.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kp.projectbookstore.R
+import com.kp.projectbookstore.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/kp/projectbookstore/activities/SearchActivity.kt
@@ -0,0 +1,20 @@
+package com.kp.projectbookstore.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kp.projectbookstore.R
+import com.kp.projectbookstore.databinding.ActivitySearchBinding
+
+class SearchActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
