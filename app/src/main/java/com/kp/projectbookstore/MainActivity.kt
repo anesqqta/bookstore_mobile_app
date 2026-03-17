e7c407384ccd43572d7a3ba6881fbf202470812e
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-03-17 22:07:08 +0200

    Етап 4: підключення навігації з головного екрана

--- app/src/main/java/com/kp/projectbookstore/MainActivity.kt
+++ app/src/main/java/com/kp/projectbookstore/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kp.projectbookstore
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -7,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.kp.projectbookstore.activities.ProfileActivity
+import com.kp.projectbookstore.activities.SearchActivity
 import com.kp.projectbookstore.adapters.BookAdapter
 import com.kp.projectbookstore.data.TestData
 import com.kp.projectbookstore.databinding.ActivityMainBinding
@@ -22,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupRecyclerBiew()
+        setupBottomNavigation()
     }
 
     private fun setupRecyclerBiew() {
@@ -31,5 +35,22 @@ class MainActivity : AppCompatActivity() {
 
         binding.rvBooks.layoutManager = LinearLayoutManager(this)
         binding.rvBooks.adapter = bookAdapter
+    }
+
+    private fun setupBottomNavigation() {
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> true
+                R.id.nav_search -> {
+                    startActivity(Intent(this, SearchActivity::class.java))
+                    true
+                }
+                R.id.nav_profile -> {
+                    startActivity(Intent(this, ProfileActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
