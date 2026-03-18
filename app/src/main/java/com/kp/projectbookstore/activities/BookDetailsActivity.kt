1c51c56865224fbb454538f6c1f41f7d4b308b1a
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-03-18 21:38:05 +0200

    Етап 5: створення Activity зі створення деталей книги

---
+++ app/src/main/java/com/kp/projectbookstore/activities/BookDetailsActivity.kt
@@ -0,0 +1,18 @@
+package com.kp.projectbookstore.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kp.projectbookstore.R
+
+class BookDetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBookDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBookDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
