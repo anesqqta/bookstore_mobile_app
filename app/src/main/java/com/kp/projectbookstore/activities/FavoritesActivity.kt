91805c61878ee4d96bb6152613ed2f7e69f701cc
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-03-24 16:44:01 +0200

    Етап 8: додавання окремого екрану обраного

---
+++ app/src/main/java/com/kp/projectbookstore/activities/FavoritesActivity.kt
@@ -0,0 +1,43 @@
+package com.kp.projectbookstore.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kp.projectbookstore.activities.BookDetailsActivity
+import com.kp.projectbookstore.adapters.BookAdapter
+import com.kp.projectbookstore.data.TestData
+import com.kp.projectbookstore.databinding.ActivityFavoritesBinding
+
+class FavoritesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFavoritesBinding
+    private lateinit var adapter: BookAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFavoritesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = BookAdapter(emptyList()) { selectedBook ->
+            val intent = Intent(this, BookDetailsActivity::class.java).apply {
+                putExtra("title", selectedBook.title)
+                putExtra("author", selectedBook.author)
+                putExtra("genre", selectedBook.genre)
+                putExtra("price", selectedBook.price)
+                putExtra("description", selectedBook.description)
+                putExtra("imageResId", selectedBook.imageResId)
+            }
+            startActivity(intent)
+        }
+
+        binding.rvFavorites.layoutManager = LinearLayoutManager(this)
+        binding.rvFavorites.adapter = adapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val favoriteBooks = TestData.books.filter { it.isFavorite }
+        adapter.updateBooks(favoriteBooks)
+    }
+}
